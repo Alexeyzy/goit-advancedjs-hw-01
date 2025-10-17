6d1f77718ecaa0812f95535fe0212d6a6321795e
Alexey <a03221983@gmail.com>
2025-10-17 13:39:34 +0300

    1

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+
+  if (email === '' || message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+  console.log(formData);
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+});
